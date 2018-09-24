fa732a74c4ed60132fa6f91ed29a2a9780a40d40
Vlanker <kinvlanknik@yandex.ru>
2018-09-24 16:43:39 +0300

    a

--- second/Second.pas
+++ second/Second.pas
@@ -13,24 +13,22 @@ type
   public
     { Public declarations }
   end;
+
   TColorType = {White, Black, Red, Green, Blue};
   TEngineType = {Diesel-Engine, Turbo-Diesel, Electric-motor, Hybrid};
   TCoachbuilderType = {Truck, Car, Waggon, Off-road, Bus};
-  TChassisType = {4, 6, 8, 10}
+  TChassisType = {4, 6, 8, 10};
+
   TAutomobile = class
   private
     Engine : TEngineType;
     Coachbuilder : TCoachbuilderType;
     Chassis : Integer;
     ColorBody : TColorType;
-
-    f
   public
-    property Enginen : String Read GetEngin Write SetEngine;
-    property Body : String Read GetBody Write SetBody;
+    constructor Create(Engine : TEngineType;);
+  public
 
-    property Enginen : String Read GetEngin Write SetEngine;
-    constructor Create(Engine : TEngineType; Body : String; NumberWheels : Integer; ColorBody : TColorType);
 
   end;  
 
@@ -44,13 +42,11 @@ implementation
 
 { TAutomobile }
 
-constructor TAutomobile.Create(Engine, Body: String; NumberWheels: Integer; ColorBody : TColorType);
-begin
-  self.Engine := Engine;
-  self.Body := Body;
-  self.NumberWheels := NumberWheels;
-  self.ColorBody := ColorBody;
-end;
+
+
+
+
+
 
 
 
