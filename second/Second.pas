9f4e0e19a2fbbbb40f97efe43d7b2a2beaf464f4
Vlanker <kinvlanknik@yandex.ru>
2018-09-24 16:37:14 +0300

    --

--- second/Second.pas
+++ second/Second.pas
@@ -13,14 +13,46 @@ type
   public
     { Public declarations }
   end;
+  TColorType = {White, Black, Red, Green, Blue};
+  TEngineType = {Diesel-Engine, Turbo-Diesel, Electric-motor, Hybrid};
+  TCoachbuilderType = {Truck, Car, Waggon, Off-road, Bus};
+  TChassisType = {4, 6, 8, 10}
+  TAutomobile = class
+  private
+    Engine : TEngineType;
+    Coachbuilder : TCoachbuilderType;
+    Chassis : Integer;
+    ColorBody : TColorType;
 
-  TAutomobile = class;
-  
+    f
+  public
+    property Enginen : String Read GetEngin Write SetEngine;
+    property Body : String Read GetBody Write SetBody;
+
+    property Enginen : String Read GetEngin Write SetEngine;
+    constructor Create(Engine : TEngineType; Body : String; NumberWheels : Integer; ColorBody : TColorType);
+
+  end;  
+
 var
   Form1: TForm1;
 
 implementation
 
 {$R *.dfm}
+
+
+{ TAutomobile }
+
+constructor TAutomobile.Create(Engine, Body: String; NumberWheels: Integer; ColorBody : TColorType);
+begin
+  self.Engine := Engine;
+  self.Body := Body;
+  self.NumberWheels := NumberWheels;
+  self.ColorBody := ColorBody;
+end;
+
+
+
 
 end.
