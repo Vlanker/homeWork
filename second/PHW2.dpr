4c12f2f0463b75fe8f08a37215c53840b7147afa
DESKTOP-RVJ25LH\vlank-h <kinvlanknik@yandex.ru>
2018-09-28 23:27:22 +0300

    2 задание готово

---
+++ second/PHW2.dpr
@@ -0,0 +1,14 @@
+program PHW2;
+
+uses
+  Vcl.Forms,
+  FUnit in 'FUnit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
